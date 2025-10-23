a49f7ed0300dd4f5e9e6317c75453c292ef8a662
Nicole Swierstra <swiersw@wwu.edu>
2025-10-22 19:10:46 -0700

    added parameters for flash project

--- modules/flash.h
+++ modules/flash.h
@@ -1,5 +1,16 @@
 #ifndef _FLASH_H_
 #define _FLASH_H_
+#include <stdint.h>
 
+/* 
+ * Unlocks the flash to be able to write to it 
+ */
+void FLASH_Init();
+
+/*
+ * Takes in a location in device sram, a page # in high cycle flash, and writes one to the other
+ * First clearing the memory with page erase, and then writing the new data from src.
+ */
+void FLASH_WriteSector(void* src, uint32_t page_start, uint32_t len);
 
 #endif
